8dccb38e37f688106318f69344fe39b7f3773df1
witch-factory <soakdma37@gmail.com>
2021-11-15 17:30:55 +0900

    1장 5강

--- sunghyun/src/App.js
+++ sunghyun/src/App.js
@@ -1,21 +1,14 @@
 import React from "react";
 import Hello from "./Hello";
+import Wrapper from "./Wrapper"
 import './App.css';
 
 function App() {
-  const name='react';
-  const style={
-    backgroundColor:'pink',
-    color:'aqua',
-    fontSize:24,
-    padding:'1rem'
-  }
   return (
-    <>
-      <Hello />
-      <div style={style}>{name}</div>
-      <div className="gray-box"></div>
-    </>
+    <Wrapper>
+      <Hello name="react" color="aqua"/>
+      <Hello color="pink" />
+    </Wrapper>
   );
 }
 
--- sunghyun/src/Hello.js
+++ sunghyun/src/Hello.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-function Hello(){
-  return <div>안녕하세요!</div>
+function Hello({color, name}){
+  return <div style={{color}}>안녕하세요! {name}</div>
+}
+
+Hello.defaultProps={
+  name:"처음 보는 사람"
 }
 
 export default Hello;
