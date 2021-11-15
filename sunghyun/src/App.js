13bf0c48f52e74e91901a96d0c405a4f8e59d463
witch-factory <soakdma37@gmail.com>
2021-11-15 20:32:36 +0900

    1장 12, 13강

--- sunghyun/src/App.js
+++ sunghyun/src/App.js
@@ -1,9 +1,64 @@
-import React from "react";
+import React, {useRef, useState} from "react";
 import UserList from "./userList";
+import CreateUser from "./CreateUser";
 
 function App() {
+  const [inputs, setInputs]=useState({
+    username:'',
+    email:''
+  });
+  const {username, email}=inputs;
+  const onChange=(e)=>{
+    const {name, value}=e.target;
+    setInputs({
+      ...inputs,
+      [name]:value
+    });
+  };
+
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ]);
+
+  const nextId=useRef(4);
+  const onCreate=()=>{
+    const newUser={
+      id:nextId.current,
+      username,
+      email
+    };
+    setUsers([...users, newUser]);
+    setInputs({
+      username:'',
+      email:''
+    });
+    nextId.current+=1;
+  }
+
   return (
-    <UserList />
+    <>
+      <CreateUser
+      username={username}
+      email={email}
+      onChange={onChange}
+      onCreate={onCreate}
+      />
+      <UserList users={users}/>
+    </>
   );
 }
 
--- sunghyun/src/userList.js
+++ sunghyun/src/userList.js
@@ -7,25 +7,7 @@ function User({user}){
     </div>
   );
 }
-function UserList() {
-  const users = [
-    {
-      id: 1,
-      username: 'velopert',
-      email: 'public.velopert@gmail.com'
-    },
-    {
-      id: 2,
-      username: 'tester',
-      email: 'tester@example.com'
-    },
-    {
-      id: 3,
-      username: 'liz',
-      email: 'liz@example.com'
-    }
-  ];
-
+function UserList({users}) {
   return (
     <div>
       {users.map((user)=>(
