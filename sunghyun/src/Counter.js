c61a13a1d68a1bb7c2405f7c668ccb64220b3839
witch-factory <soakdma37@gmail.com>
2021-11-17 02:21:43 +0900

    1장 끝

--- sunghyun/src/Counter.js
+++ sunghyun/src/Counter.js
@@ -1,32 +1,30 @@
-import React, {useReducer, useState} from "react";
+import React, {Component} from "react";
 
-function reducer(state, action){
-  switch(action.type){
-    case 'INCREMENT':
-      return state+1;
-    case 'DECREMENT':
-      return state-1;
-    default:
-      return state;
-  }
-}
+class Counter extends Component {
+  state = {
+    counter: 0,
+  };
+  handleIncrease = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  };
 
-function Counter(){
-  const [number, dispatch]=useReducer(reducer, 0);
-  const onIncrease=()=>{
-    dispatch({type:'INCREMENT'});
-  }
-  const onDecrease=()=>{
-    dispatch({type:'DECREMENT'});
-  }
+  handleDecrease = () => {
+    this.setState({
+      counter: this.state.counter - 1
+    });
+  };
 
-  return (
-    <div>
-      <h1>{number}</h1>
-      <button onClick={onIncrease}>1 증가</button>
-      <button onClick={onDecrease}>1 감소</button>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <h1>{this.state.counter}</h1>
+        <button onClick={this.handleIncrease}>+1</button>
+        <button onClick={this.handleDecrease}>-1</button>
+      </div>
+    );
+  }
 }
 
 export default Counter;
--- sunghyun/src/index.js
+++ sunghyun/src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Counter from "./Counter";
+import Hello from "./Hello";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Counter />
   </React.StrictMode>,
   document.getElementById('root')
 );
