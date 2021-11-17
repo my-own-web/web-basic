cb6d0a68202d370cc6f80b1342b0cd2fc2e79577
witch-factory <soakdma37@gmail.com>
2021-11-17 13:24:34 +0900

    useparams, uselocation in react router

--- sunghyun/src/App.js
+++ sunghyun/src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import styled, {createGlobalStyle} from 'styled-components';
-import TodoTemplate from "./components/TodoTemplate.js";
-import TodoListHeader from "./components/TodoListHeader";
-import TodoList from "./components/TodoList";
-import TodoListAdd from "./components/TodoListAdd";
-import {TodoProvider} from "./TodoContext";
 import {Route, Routes, Link} from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
+import Profile from "./Profile";
 
 const GlobalStyle=createGlobalStyle`
   body{
     background: #f8f0fc;
   }
-`
+`;
 
 const App=()=>{
   return (
     <div>
+      <GlobalStyle/>
       <ul>
         <li>
           <Link to='/'>메인 페이지</Link>
@@ -29,6 +26,7 @@ const App=()=>{
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/about' element={<About />} />
+        <Route path='/profiles/:username' element={<Profile />} />
       </Routes>
     </div>
   );
