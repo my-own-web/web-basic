dd01bb4d5e966879213d2921b479c6935cb9069e
lectura7942 <81620001+lectura7942@users.noreply.github.com>
2021-11-24 04:15:18 +0900

    ADD design

--- jieun-login/src/App.js
+++ jieun-login/src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './Login';
+import {createGlobalStyle} from 'styled-components';
 
+// createGlobalStyle: 글로벌 스타일
+const GlobalStyle = createGlobalStyle`
+  body{
+    background: #e9ecef;
+  }
+`
 function App() {
   return (
-    <Login />
+    <>
+    <GlobalStyle />
+    <Login/>
+    </>
   );
 }
 
