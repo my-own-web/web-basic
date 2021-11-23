fab7ef4aa847807d44268df97965b06349c3a1a5
witch-factory <soakdma37@gmail.com>
2021-11-23 19:40:22 +0900

    로그인 버튼 모양 제작

--- sunghyun/src/LoginPage.js
+++ sunghyun/src/LoginPage.js
@@ -1,6 +1,7 @@
 import React from "react";
-import styled, {createGlobalStyle} from "styled-components";
+import styled, {createGlobalStyle, css} from "styled-components";
 import TodoListTemplate from "./components/TodoListTemplate";
+import {darken} from "polished";
 
 const GlobalStyle=createGlobalStyle`
   body{
@@ -33,7 +34,26 @@ const LoginInputForm=styled.input`
   width:20rem;
   height:2.5rem;
   font-size:2.5rem;
-  type:${props=>props.type && 'text'}
+  type:${props=>props.type && 'text'};
+`;
+
+const LoginButton=styled.button`
+  width:${props=>props.width};
+  height:${props=>props.height};
+  background: ${props=>props.background};
+  color:white;
+  font-size:1.5rem;
+  border-radius: 7px;
+  border:none;
+  
+  &:hover{
+    ${props=>{
+      return css`
+        background:${darken(0.1, props.background)};
+      `;
+    }
+    }
+  }
 `;
 
 const LoginPage=()=>{
@@ -48,6 +68,7 @@ const LoginPage=()=>{
         <LoginInputForm />
         <h1>PASSWORD</h1>
         <LoginInputForm type="password"/>
+        <LoginButton width='8rem' height='3rem' background='#e599fc'>로그인</LoginButton>
       </LoginFormContainer>
     </TodoListTemplate>
   )
