6ba5f6382fa3e4db7a11bf506ae3fdd57d6d5115
lectura7942 <81620001+lectura7942@users.noreply.github.com>
2021-11-24 02:47:47 +0900

    fix: validity check delay 해결

--- jieun-login/src/Login.js
+++ jieun-login/src/Login.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import styled from 'styled-components';
 
-// dbg example info
+// (dbg) example info
 const info = [{
     id: 'id',
     password: 'password'
@@ -15,6 +15,8 @@ const info = [{
     password:'b'
 }];
 
+// info 배열에 id, password가 존재하면 true
+// 없으면 false 반환
 function isValid({id, password}){
     const index = info.find((element)=>element.id===id);
     if(index && index.password===password) return true;
@@ -22,14 +24,17 @@ function isValid({id, password}){
 }
 
 function Login() {
+    // 입력
     const [inputs, setInputs]=useState({
         id: '',
         password: ''
     });
+    // 제출한 id, password
     const [submits, setSubmits]=useState({
         id:'',
         password:''
     });
+    // 제출한 id, password가 info 배열에 존재하면 true
     const [valid, setValid]=useState(false);
 
     const onChange =(e)=>{
@@ -40,10 +45,10 @@ function Login() {
         });
     };
 
-    const onSubmit= (e)=>{
+    // 로그인 버튼 누르면 그때까지 입력받은 id, password를 제출, validity check
+    const onSubmit= ()=>{
         setSubmits({id:inputs.id, password:inputs.password});
-        // setValid(info.find((element)=>{return element.id === submits.id && element.password === submits.password}));
-        setValid(isValid(submits));
+        setValid(isValid(inputs));
         setInputs({id:'', password:''});
     };
 
@@ -51,11 +56,19 @@ function Login() {
         <>
             <input name="id" placeholder="ID" onChange={onChange} value={inputs.id}/>
             <input name="password" placeholder="PASSWORD" onChange={onChange} value={inputs.password}/>
-            <button onClick={onSubmit}>확인</button>
+            <button onClick={onSubmit}>로그인</button>
             <div><b>ID:</b> {submits.id}</div>
             <div><b>PASSWORD:</b> {submits.password}</div>
             <div style={{color: valid? "green":
         "red"}}>{valid? 'valid':'Invalid'}</div>
+
+        {/*dbg*/}
+        <p style={{color:"blue"}}>
+        <div><b>info 배열: id password</b></div>
+        <div>a b</div>
+        <div>jieun kwon</div>
+        <div>id password</div>
+        </p>
         </>
     )
 }
