442392e6144193968e5416a6139220a906153ed0
lectura7942 <81620001+lectura7942@users.noreply.github.com>
2021-11-24 02:14:00 +0900

    Feat: ID, PASSWORD 확인 기능 추가
    -확인 button 누르면 입력한 ID,PASSWORD 제출
    -제출한 ID, PASSWORD가 info 배열에 존재하는지 확인
    
    bug:
    - 확인 버튼을 두 번 눌러야지 valid,invalid 값 바뀜

--- jieun-login/src/Login.js
+++ jieun-login/src/Login.js
@@ -1,11 +1,36 @@
 import React, {useState} from 'react';
+import styled from 'styled-components';
+
+// dbg example info
+const info = [{
+    id: 'id',
+    password: 'password'
+},
+{
+    id: 'jieun',
+    password: 'kwon'
+},
+{
+    id: 'a',
+    password:'b'
+}];
+
+function isValid({id, password}){
+    const index = info.find((element)=>element.id===id);
+    if(index && index.password===password) return true;
+    else return false; 
+}
 
 function Login() {
     const [inputs, setInputs]=useState({
         id: '',
         password: ''
     });
-    const {id, password}=inputs;
+    const [submits, setSubmits]=useState({
+        id:'',
+        password:''
+    });
+    const [valid, setValid]=useState(false);
 
     const onChange =(e)=>{
         const {value, name}=e.target;
@@ -15,13 +40,22 @@ function Login() {
         });
     };
 
+    const onSubmit= (e)=>{
+        setSubmits({id:inputs.id, password:inputs.password});
+        // setValid(info.find((element)=>{return element.id === submits.id && element.password === submits.password}));
+        setValid(isValid(submits));
+        setInputs({id:'', password:''});
+    };
+
     return (
         <>
-            <input name="id" placeholder="ID" onChange={onChange} value={id}/>
-            <input name="password" placeholder="PASSWORD" onChange={onChange} value={password}/>
-            <button onClick={}>확인</button>
-            <div><b>ID:{id}</b></div>
-            <div><b>PASSWORD:{password}</b></div>
+            <input name="id" placeholder="ID" onChange={onChange} value={inputs.id}/>
+            <input name="password" placeholder="PASSWORD" onChange={onChange} value={inputs.password}/>
+            <button onClick={onSubmit}>확인</button>
+            <div><b>ID:</b> {submits.id}</div>
+            <div><b>PASSWORD:</b> {submits.password}</div>
+            <div style={{color: valid? "green":
+        "red"}}>{valid? 'valid':'Invalid'}</div>
         </>
     )
 }
