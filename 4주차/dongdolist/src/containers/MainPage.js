558862650c7849f8318b6875fc3155a1da0501aa
nant0313 <nant0313@gmail.com>
2021-11-24 17:41:14 +0900

    Add dongdolist server code

--- 4주차/dongdolist/src/containers/MainPage.js
+++ 4주차/dongdolist/src/containers/MainPage.js
@@ -5,8 +5,8 @@ import TodoHead from "../components/TodoHead";
 const TodoTemplateBlock = styled.div`
   width: 512px;
   height: 768px;
-
-  position: relative; /* 추후 박스 하단에 추가 버튼을 위치시키기 위한 설정 */
+  /* 추후 박스 하단에 추가 버튼을 위치시키기 위한 설정 */
+  position: relative; 
   background: white;
   border-radius: 16px;
   box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.04);
