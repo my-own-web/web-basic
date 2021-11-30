38c8dd901fab0d372654b1aa0df0b60235924bad
lectura7942 <81620001+lectura7942@users.noreply.github.com>
2021-12-01 02:05:11 +0900

    feat: edit todolist

--- jieun-todolist/src/components/TodoItem.js
+++ jieun-todolist/src/components/TodoItem.js
@@ -1,8 +1,20 @@
 // 할 일 항목 보여줌
-import React from 'react';
+import React, { useState } from 'react';
 import styled, { css } from 'styled-components';
-import { MdDone, MdDelete } from 'react-icons/md';
+import { MdDone, MdDelete, MdEdit } from 'react-icons/md';
+import { BiXCircle } from 'react-icons/bi';
 import { useTodoDispatch } from '../TodoContext';
+
+const QuitEdit = styled.div`
+  display: flex;
+  color: #dee2e6;
+  font-size: 24px;
+  cursor: pointer;
+  &:hover {
+    color: #ff6b6b;
+  }
+  display: none;
+`;
 
 const Remove = styled.div`
   display: flex;
@@ -24,6 +36,9 @@ const TodoItemBlock = styled.div`
   padding-bottom: 12px;
   &:hover { // 커서가 위에 있을 때 Remove 컴포넌트 보임.
     ${Remove} {
+      display: initial; // initial: 브라우저가 지정한 속성의 초기값 적용
+    }
+    ${QuitEdit}{
       display: initial;
     }
   }
@@ -59,18 +74,65 @@ const Text = styled.div`
     `}
 `;
 
+const EditInput = styled.input`
+  padding: 10px;
+  margin-right:10px;
+  border-radius: 4px;
+  border: 1px solid #dee2e6;
+  width: 80%;
+  outline: none;
+  font-size: 18px;
+  box-sizing: border-box;
+`
+
 function TodoItem({ id, done, text }) {
-const dispatch = useTodoDispatch();
-const onToggle = () => dispatch({type: 'TOGGLE', id});
-const onRemove = () => dispatch({type: 'REMOVE', id});
+  const [input, setInput] = useState(text);
+  const onChange = e => {
+    return setInput(e.target.value);
+  };
+
+  const dispatch = useTodoDispatch();
+  const onToggle = () => dispatch({ type: 'TOGGLE', id });
+  const onRemove = () => dispatch({ type: 'REMOVE', id });
+  const onEdit = () => {
+    dispatch({
+      type: 'EDIT',
+      id,
+      text: input
+    });
+  };
+
+  const [edit, setEdit] = useState(false);
+  const onStartEdit = () => {
+    return (
+      setEdit(!edit)
+    );
+  };
+
+  const onKeyPress=(e)=>{
+    if(e.key=='Enter'){
+      onEdit();
+      onStartEdit();
+    }
+  };
 
   return (
     <TodoItemBlock>
       <CheckCircle done={done} onClick={onToggle}>{done && <MdDone />}</CheckCircle>
-      <Text done={done}>{text}</Text>
-      <Remove onClick={onRemove}>
+      {edit? 
+      <>
+      <EditInput value={input} onChange={onChange} onKeyPress={onKeyPress}/>
+       <QuitEdit onClick={onStartEdit}>
+        <BiXCircle />
+      </QuitEdit>
+      </>
+      : <>
+      <Text done={done} onClick={onStartEdit}>{text}</Text>
+            <Remove onClick={onRemove}>
         <MdDelete /> {/** 쓰레기통 아이콘 */}
       </Remove>
+      </>
+      }     
     </TodoItemBlock>
   );
 }
