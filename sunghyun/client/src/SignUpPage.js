2d5e3d5daebc3a22f906cc1f75b2f489c630fba5
witch-factory <soakdma37@gmail.com>
2022-01-05 01:54:03 +0900

    회원가입 페이지 껍데기 완성

--- sunghyun/client/src/SignUpPage.js
+++ sunghyun/client/src/SignUpPage.js
@@ -6,6 +6,8 @@ import {ColumnContainer, RowContainer} from "./components/Container";
 import Input from "./components/Input";
 import InputPositioner from "./components/InputPositioner";
 import InputInfo from "./components/InputInfo";
+import styled from "styled-components";
+import LoginButton from "./components/LoginButton";
 
 const GlobalStyle=createGlobalStyle`
   body{
@@ -13,27 +15,29 @@ const GlobalStyle=createGlobalStyle`
   }
 `;
 
+const ConstraintInfo=styled.h5`
+  margin:0;
+`;
+
 const SignUpInput=()=>{
   return (
     <ColumnContainer>
-      <RowContainer>
-        <InputInfo>아이디</InputInfo>
+        <InputInfo fontSize='1.5rem'>USERNAME</InputInfo>
+        <InputPositioner>
+          <Input name='username'/>
+        </InputPositioner>
+      <ConstraintInfo>유저네임은 10자 미만의 영어 소문자와 숫자로 이루어져야 합니다.</ConstraintInfo>
+        <InputInfo fontSize='1.5rem'>PASSWORD</InputInfo>
+        <InputPositioner>
+          <Input name='password'/>
+        </InputPositioner>
+      <ConstraintInfo>비밀번호는 10자 미만의 영어 소문자와 숫자로 이루어져야 합니다.</ConstraintInfo>
+        <InputInfo fontSize='1.5rem'>VERIFY PASSWORD</InputInfo>
         <InputPositioner>
           <Input name='id'/>
         </InputPositioner>
-      </RowContainer>
-      <RowContainer>
-        <InputInfo>비밀번호</InputInfo>
-        <InputPositioner>
-          <Input name='id'/>
-        </InputPositioner>
-      </RowContainer>
-      <RowContainer>
-        <InputInfo>비밀번호 확인</InputInfo>
-        <InputPositioner>
-          <Input name='id'/>
-        </InputPositioner>
-      </RowContainer>
+      <ConstraintInfo>패스워드를 한 번 더 입력해 주세요.</ConstraintInfo>
+      <LoginButton width='8rem' height='3rem' background='#da77f2'>회원가입</LoginButton>
     </ColumnContainer>
   )
 }
--- sunghyun/client/src/components/InputInfo.js
+++ sunghyun/client/src/components/InputInfo.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const InputInfo=styled.h2`
   margin:5px;
   color:#862e9c;
-  font-size:2rem;
+  font-size:${props=>props.fontSize || '2rem'};
 `;
 
 export default InputInfo;
