d7946d14e52105640abe0b45ea28b40f06b4bba4
juk1329 <juk2717an@gmail.com>
2022-01-07 21:59:55 +0900

    todolist db 연동 중(미완성)

---
+++ jungkyu_todolist/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import TodoTemplate from './components/TodoTemplate';
+import TodoHead from './components/TodoHead';
+import TodoList from './components/TodoList';
+import TodoCreate from './components/TodoCreate';
+import { TodoProvider } from './TodoContext';
+//import axios from 'axios';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
+  }
+`;
+
+
+function App() {
+  
+  return (
+    <TodoProvider>
+      <GlobalStyle />
+      <TodoTemplate>
+        <TodoHead/>
+        <TodoList/>
+        <TodoCreate/>
+      </TodoTemplate>
+    </TodoProvider>
+  );
+}
+
+export default App;
